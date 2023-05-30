ba9856563a2f46789a629e125d25ed86eb0473e4
nockoy <clarinetbroken0819@gmail.com>
2023-05-30 18:20:14 +0900

    デプロイできるか確認

---
+++ hackathon/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"db/controller"
+	"db/dao"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+)
+
+// ① GoプログラムからMySQLへ接続
+func init() {
+	dao.DBInit()
+}
+
+// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		controller.SearchUser(w, r)
+
+	case http.MethodPost:
+		controller.RegisterUser(w, r)
+
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		controller.GetMessage(w, r)
+	case http.MethodPost:
+		controller.SendMessage(w, r)
+
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func roomHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		controller.RegisterRoom(w, r)
+
+	default:
+		log.Printf("fail: HTTP Method is %s\n", r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+func main() {
+
+	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
+	http.HandleFunc("/user", userHandler)
+	http.HandleFunc("/message", messageHandler)
+	http.HandleFunc("/newroom", roomHandler)
+
+	// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
+	closeDBWithSysCall()
+
+	// 8000番ポートでリクエストを待ち受ける
+	log.Println("Listening...")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
+func closeDBWithSysCall() {
+	dao.DBClose()
+}
