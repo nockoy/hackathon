b1b92d8c7dec3fbd865ee42ba2460d0748c8c3e0
nockoy <clarinetbroken0819@gmail.com>
2023-05-30 16:33:58 +0900

    main.go内の不要なコードを削除したものの、不要でなかったことがわかったため戻した

--- hackathon/main.go
+++ hackathon/main.go
@@ -8,10 +8,13 @@ import (
 	"net/http"
 )
 
+// ① GoプログラムからMySQLへ接続
+
 func init() {
 	dao.DBInit()
 }
 
+// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
 func userHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -55,8 +58,22 @@ func roomHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	// ② /userでリクエストされたらnameパラメーターと一致する名前を持つレコードをJSON形式で返す
 	http.HandleFunc("/user", userHandler)
 	http.HandleFunc("/message", messageHandler)
 	http.HandleFunc("/newroom", roomHandler)
 
+	// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
+	closeDBWithSysCall()
+
+	// 8000番ポートでリクエストを待ち受ける
+	log.Println("Listening...")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// ③ Ctrl+CでHTTPサーバー停止時にDBをクローズする
+func closeDBWithSysCall() {
+	dao.DBClose()
 }
